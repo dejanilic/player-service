b6f4f1a1391e2a5fa64dfb92537d4c95549edab8
ilicdejan <ilicdejan1997@gmail.com>
2023-02-08 10:36:58 +0100

    Doradjeni rest testovi

--- src/test/java/com/holycode/playerservice/rest/PlayerControllerTest.java
+++ src/test/java/com/holycode/playerservice/rest/PlayerControllerTest.java
@@ -1,20 +1,23 @@
 package com.holycode.playerservice.rest;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.holycode.playerservice.PlayerServiceApplicationTests;
 import com.holycode.playerservice.domain.Player;
+import com.holycode.playerservice.service.PlayerService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(PlayerController.class)
 public class PlayerControllerTest extends PlayerServiceApplicationTests {
@@ -25,44 +28,42 @@ public class PlayerControllerTest extends PlayerServiceApplicationTests {
     private MockMvc mvc;
 
     @MockBean
-    private PlayerController playerController;
+    private PlayerService playerService;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    // Improvement: nisam siguran da li je ovo okej način za testiranje rest-a, vrv. ima neki bolji način za koji nisam čuo
     @Test
     public void testGetPlayerInfo() throws Exception {
-        Player player = new Player(1L, "Dejan", 1L);
+        given(playerService.getInfo(any())).willReturn(new Player(1L, "Dejan", 10L));
 
-        ResultActions result = mvc.perform(MockMvcRequestBuilders.get("/player")
-                .param("playerId", "1")
-                .content(objectMapper.writeValueAsString(player))
-                .contentType(MediaType.APPLICATION_JSON));
-
-        result.andExpect(status().isOk());
+        mvc.perform(MockMvcRequestBuilders.get("/player")
+                        .param("playerId", "1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", is("Dejan")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.gameId", is(10)));
     }
 
     @Test
     public void testRegisterPlayer() throws Exception {
-        Player player = new Player(1L, "Dejan", 1L);
+        Player player = new Player(1L, "Dejan", 10L);
 
-        ResultActions result = mvc.perform(post("/register")
-                .content(objectMapper.writeValueAsString(player))
-                .contentType(MediaType.APPLICATION_JSON));
+        given(playerService.register(any())).willReturn(player);
 
-        result.andExpect(status().isOk());
+        mvc.perform(MockMvcRequestBuilders.post("/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(player)))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
     }
 
     @Test
     public void testDeletePlayer() throws Exception {
-        Player player = new Player(1L, "Dejan", 1L);
 
-        ResultActions result = mvc.perform(delete("/player")
-                .param("playerId", "1")
-                .content(objectMapper.writeValueAsString(player))
-                .contentType(MediaType.APPLICATION_JSON));
-
-        result.andExpect(status().isOk());
+        mvc.perform(MockMvcRequestBuilders.delete("/player")
+                        .param("playerId", "1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
